15aed381693acb2bf8f39e9c8b8d76159e184c8b
tablonimus <lautamartin1995@gmail.com>
2022-10-14 13:27:37 -0300

    con push

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,28 @@
 import React from "react";
-import ac from "../../assets/images/ac.png"
+import ac from "../../assets/images/ac.png";
 import { Navbar } from "flowbite-react";
 
 export default function NavBar() {
   return (
-    <Navbar class="flex bg-white items-center w-full fixed" fluid={true} rounded={true}>
+    <Navbar
+      class="flex bg-white items-center w-full fixed"
+      fluid={true}
+      rounded={true}
+    >
       <Navbar.Brand href="">
         <span className="flex items-center ml-2 self-center whitespace-nowrap text-xl font-semibold text-white">
           <img src={ac} className="h-20 rounded-full flex" alt="Logo" />
           <h1 className="text-black font-bold font-serif text-md text-sm ml-2 mr-5">
             {" "}
-
           </h1>
         </span>
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
-        <Navbar.Link href="/home">
+        <Navbar.Link href="/">
           <span className="text-black hover:text-orange-600">Inicio</span>
         </Navbar.Link>
-        <Navbar.Link href="/home">
+        {/* <Navbar.Link href="/home">
           {" "}
           <span className="text-black hover:text-orange-600">Fechas</span>{" "}
         </Navbar.Link>
@@ -38,7 +41,7 @@ export default function NavBar() {
         <Navbar.Link href="/navbars">
           {" "}
           <span className="text-black hover:text-orange-600">Nada</span>{" "}
-        </Navbar.Link>
+        </Navbar.Link> */}
       </Navbar.Collapse>
     </Navbar>
   );
